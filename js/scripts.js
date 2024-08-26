e53efc96b57f50cc5305298db38438a567f329ae
Kayky <133372865+Kaykyvd0@users.noreply.github.com>
2024-08-26 17:23:50 -0300

    Atalização no leyout
    1.Mandar mais texto p kayky
    2.Adicionar imagem do motor e cerca elétrica no carrossel 
    3.Sobre nós com opacidade (cinza com o fundo da cor do layout)
    4.Recortar a imagem da logo inicial, tirar as bordas
    5.Botão p subir p início do site

--- js/scripts.js
+++ js/scripts.js
@@ -23,4 +23,17 @@ function showSlides() {
 }
 
 
+window.onscroll = function() {
+  const backToTopButton = document.getElementById('backToTop');
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      backToTopButton.style.display = "block";
+  } else {
+      backToTopButton.style.display = "none";
+  }
+};
+
+
+document.getElementById('backToTop').addEventListener('click', function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
 
